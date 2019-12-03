c7e97fe4bb0da8a5c87201f772a61b00d0c80444
Andrew Paulino <andrewpaulino46@gmail.com>
2019-12-03 00:21:01 -0800

    Added registering moves

--- WebSocket.js
+++ WebSocket.js
@@ -1,64 +1,90 @@
 var clients = {};
 var gameSessions = {};
-var WebSocketServer = require("ws").Server;
-var http = require("http");
-var express = require("express");
-var _utils = require("./utils.js");
-var uuidv4 = require("uuid/v4");
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
+var _utils = require('./utils.js');
+var uuidv4 = require('uuid/v4');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + '/'));
 
 var server = http.createServer(app);
 server.listen(port);
 
-console.log("http server listening on %d", port);
+console.log('http server listening on %d', port);
 
 var wss = new WebSocketServer({ server: server });
-console.log("websocket server created");
+console.log('websocket server created');
 
-wss.on("connection", function(ws) {
-  let id = uuidv4();
-  clients[id] = ws;
-  ws.on("message", function(message) {
-    switch (message.match(/<(.*?)>/)[1]) {
-      case "new":
-        intializeNewGame(id);
-        break;
-      case "connect":
-        connectPlayerToGame(message.match(/\(([^)]+)\)/)[1], id);
-        break;
-      default:
-        console.log("NOT FOUND");
-    }
-  });
+wss.on('connection', function(ws) {
+    let id = uuidv4();
+    clients[id] = ws;
+    ws.on('message', function(message) {
+        switch (message.match(/<(.*?)>/)[1]) {
+            case 'new':
+                intializeNewGame(id);
+                break;
+            case 'connect':
+                connectPlayerToGame(message.match(/\(([^)]+)\)/)[1], id);
+                break;
+            case 'movePlayer1':
+                registerMove(message.match(/\(([^)]+)\)/)[1], 1, message.substr(message.indexOf("{") + 1, message.indexOf("}") - message.indexOf("{") - 1), message.substr(message.indexOf("(") + 1, message.indexOf(")") - message.indexOf("(") - 1));
+                break;
+            case 'movePlayer2':
+                registerMove(message.match(/\(([^)]+)\)/)[1], 2, message.substr(message.indexOf("{") + 1, message.indexOf("}") - message.indexOf("{") - 1), message.substr(message.indexOf("(") + 1, message.indexOf(")") - message.indexOf("(") - 1));
+                break;
+            default:
+                console.log('NOT FOUND');
+        }
+    });
 
-  console.log("websocket connection open");
+    console.log('websocket connection open');
 
-  ws.on("close", function() {
-    console.log("websocket connection close");
-    clearInterval(id);
-  });
+    ws.on('close', function() {
+        console.log('websocket connection close');
+        clearInterval(id);
+    });
 });
 
+function registerMove(gameCode, clientNumber, movement, isHit) {
+      let session = gameSessions[gameCode];
+
+      if (clientNumber === 1) {
+        if (isHit) {
+          healthPlayerTwo -= 25;
+        }
+        clients[session.clientTwo].send(`<update_movement> (playerOne) {${movement}}`)
+      } else {
+        if (isHit) {
+          healthPlayerOne -= 25;
+        }
+        clients[session.clientOne].send(`<update_movement> (playerTwo) {${movement}}`)
+      }
+}
+
 function intializeNewGame(id) {
-  let gameCode = _utils.getRandomFourNumbers();
-  gameSessions[gameCode] = {
-    clientOne: id,
-    clientTwo: "waiting",
-    healthPlayerOne: 100,
-    healthPlayerTwo: 100,
-  };
-  clients[id].send(`<gamecode> (${gameCode})`);
-  console.log(
-    `New Game Session Created By, UserUUID: ${id} With Code of ${gameCode}`
-  );
+    let gameCode = _utils.getRandomFourNumbers();
+    gameSessions[gameCode] = {
+        clientOne: id,
+        clientTwo: 'waiting',
+        healthPlayerOne: 100,
+        healthPlayerTwo: 100
+    };
+    clients[id].send(`<gamecode> (${gameCode})`);
+    console.log(`New Game Session Created By, UserUUID: ${id} With Code of ${gameCode}`);
 }
 
 function connectPlayerToGame(gameCode, clientTwoId) {
-  const currentSession = gameSessions[gameCode];
-  currentSession.clientTwo = clientTwoId;
-  clients[currentSession.clientTwo].send("<game> (start)");
-  clients[currentSession.clientOne].send("<game> (start)");
+    const currentSession = gameSessions[gameCode];
+    currentSession.clientTwo = clientTwoId;
+    clients[currentSession.clientTwo].send('<game> (start)');
+    clients[currentSession.clientOne].send('<game> (start)');
 }
+
+
+//Schema:
+//<clientNumber> [gameCode] {movement} (collisionDetected) **if collison detect opposite client health depleted;
+
+//<movePlayer1> [gameCode] {right_3} (false)
